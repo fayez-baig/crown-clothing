2d3d2d4b5e0c7125b31f509d4149e34b18925808
fayez baig <coderninja16@gmail.com>
2020-05-30 18:46:21 +0530

    crown-clothing added new components

---
+++ src/components/pages/homepage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Directory from '../../../components/directory/Directory'
+import  './homepage.scss'
+
+const Hompage  = () => (
+    <div className = "homepage">
+        <Directory/>
+    </div>
+     
+)
+
+export default Hompage;
