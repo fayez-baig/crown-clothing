363e86d6789e3e30af8085b78fa18e2700b0c7df
fayez baig <coderninja16@gmail.com>
2020-05-31 20:18:39 +0530

    added shop page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom'
+import ShopPage from './components/pages/shop/ShopPage'
 import HomePage from './components/pages/homepage/HomePage'
 import './App.css';
 
+const HatsPage = () => (
+  <div>
+    <p1>Hats</p1>
+  </div>
+)
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <Switch>
+      <Route exact path="/" component= {HomePage}/>
+      <Route exact path="/shop" component= {ShopPage}/>
+      </Switch>
+
     </div>
   );
 }
--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import {withRouter} from 'react-router-dom';
 import './menu-item.scss'
 
-const MenuItem = ({title, imageUrl, size}) => (
-   <div className={`${size} menu-item`}>
+const MenuItem = ({title, imageUrl, size,history,linkUrl,match}) => {
+    console.log(match);
+    console.log(linkUrl);
+    return (<div className={`${size} menu-item`} onClick= {()=> history.push(`${match.url}${linkUrl}`)}>
     <div className="background-image"style= {{
         backgroundImage: `url(${imageUrl})`
     }} />
@@ -10,7 +13,7 @@ const MenuItem = ({title, imageUrl, size}) => (
                     <h1 className="title">{title}</h1>
                     <span className="subtitl">SHOP</span>
                 </div>
-            </div>
-);
+            </div>);
+};
 
-export default MenuItem;
+export default  withRouter(MenuItem);
