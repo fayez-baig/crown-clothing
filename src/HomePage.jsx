1e0a75228dc8039103c194ca1d86050e8650a7c2
fayez baig <coderninja16@gmail.com>
2020-06-12 16:08:17 +0530

    removed warnings

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,42 +1,41 @@
-import React, { Component } from 'react';
-import  './homepage.scss'
+import React from "react";
+import "./homepage.scss";
 
-const Hompage  = () => (
-    <div className = "homepage">
-        <div className="directory-menu">
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">HATS</h1>
-                    <span className="subtitl">SHOP</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">JACKETS</h1>
-                    <span className="subtitl">SHOP</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">SNEAKERS</h1>
-                    <span className="subtitl">SHOP</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">WOMENS</h1>
-                    <span className="subtitl">SHOP</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">MENS</h1>
-                    <span className="subtitl">SHOP</span>
-                </div>
-            </div>
+const Hompage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitl">SHOP</span>
         </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">JACKETS</h1>
+          <span className="subtitl">SHOP</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">SNEAKERS</h1>
+          <span className="subtitl">SHOP</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">WOMENS</h1>
+          <span className="subtitl">SHOP</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">MENS</h1>
+          <span className="subtitl">SHOP</span>
+        </div>
+      </div>
     </div>
-     
-)
+  </div>
+);
 
 export default Hompage;
--- src/components/pages/homepage/HomePage.jsx
+++ src/components/pages/homepage/HomePage.jsx
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
-import Directory from '../../../components/directory/Directory'
-import  './homepage.scss'
+import React from "react";
+import Directory from "../../../components/directory/Directory";
+import "./homepage.scss";
 
-const Hompage  = () => (
-    <div className = "homepage">
-        <Directory/>
-    </div>
-     
-)
+const Hompage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
 
 export default Hompage;
